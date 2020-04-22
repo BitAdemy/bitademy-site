113ffc2de7fa16e9fd7346f0b11a1113dfa83f69
Alberto Basalo <albertobasalo@hotmail.com>
2020-04-22 11:34:11 +0200

    chore: 🚄 no offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  pathPrefix: "/",
-  siteMetadata: require("./site-metadata.json"),
+  pathPrefix: '/',
+  siteMetadata: require('./site-metadata.json'),
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-source-data`,
-    "gatsby-plugin-netlify-cache",
-    "gatsby-plugin-sitemap",
-    `gatsby-plugin-offline`,
-    "gatsby-plugin-robots-txt",
+    'gatsby-plugin-netlify-cache',
+    'gatsby-plugin-sitemap',
+    // `gatsby-plugin-offline`,
+    'gatsby-plugin-robots-txt',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/static/images`,
-      },
+        path: `${__dirname}/static/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -21,62 +21,64 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages`,
-      },
+        path: `${__dirname}/src/pages`
+      }
     },
     {
       resolve: `gatsby-plugin-stackbit-static-sass`,
       options: {
         inputFile: `${__dirname}/src/sass/main.scss`,
-        outputFile: `${__dirname}/public/assets/css/main.css`,
-      },
+        outputFile: `${__dirname}/public/assets/css/main.css`
+      }
     },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [`gatsby-remark-component`,
-        {
-          resolve: "gatsby-remark-external-links",
-          options: {
-            target: "_blank",
-            rel: "nofollow noopener noreferrer"
+        plugins: [
+          `gatsby-remark-component`,
+          {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'nofollow noopener noreferrer'
+            }
           }
-        }],
-      },
+        ]
+      }
     },
     {
       resolve: `gatsby-remark-page-creator`,
-      options: {},
+      options: {}
     },
     {
       resolve: `@stackbit/gatsby-plugin-menus`,
       options: {
         sourceUrlPath: `fields.url`,
         pageContextProperty: `menus`,
-        menus: require("./src/data/menus.json"),
-      },
+        menus: require('./src/data/menus.json')
+      }
     },
     {
       resolve: `gatsby-plugin-gtag`,
       options: {
         trackingId: `UA-153738218-1`,
         head: false,
-        anonymize: false,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `bitAdemy`,
-        short_name: `bitAdemy`,
-        start_url: `/`,
-        background_color: `#0098EE`,
-        theme_color: `#49FCD4`,
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: `standalone`,
-        icon: `static/images/ba.png`, // This path is relative to the root of the site.
-      },
-    },
-  ],
+        anonymize: false
+      }
+    }
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `bitAdemy`,
+    //     short_name: `bitAdemy`,
+    //     start_url: `/`,
+    //     background_color: `#0098EE`,
+    //     theme_color: `#49FCD4`,
+    //     // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+    //     // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+    //     display: `standalone`,
+    //     icon: `static/images/ba.png`, // This path is relative to the root of the site.
+    //   },
+    // },
+  ]
 };
