c2f90ddd8e15cf4be747de6b9ce0c4f47dcc58fd
Alberto Basalo <albertobasalo@hotmail.com>
2020-04-24 12:15:47 +0200

    feat: 🚄 dns-prefetch preconnect

--- src/components/core/Scripts.js
+++ src/components/core/Scripts.js
@@ -8,10 +8,16 @@ export default function Scripts(props) {
       <link rel="dns-prefetch" href="https://cdn.convertbox.com"></link>
       <link rel="dns-prefetch" href="https://fonts.googleapis.com"></link>
       <link rel="dns-prefetch" href="https://fonts.gstatic.com"></link>
+      <link rel="dns-prefetch" href="https://cdn.jsdelivr.net"></link>
+      <link rel="dns-prefetch" href="https://www.googletagmanager.com"></link>
+      <link rel="dns-prefetch" href="https://www.google-analytics.com"></link>
       <link rel="preconnect" href="https://app.convertbox.com"></link>
       <link rel="preconnect" href="https://cdn.convertbox.com"></link>
       <link rel="preconnect" href="https://fonts.googleapis.com"></link>
       <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+      <link rel="preconnect" href="https://cdn.jsdelivr.net"></link>
+      <link rel="preconnect" href="https://www.googletagmanager.com"></link>
+      <link rel="preconnect" href="https://www.google-analytics.com"></link>
       <script
         async
         src={convertBox.src}
