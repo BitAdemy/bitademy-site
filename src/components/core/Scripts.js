df1aa39cc22a624ef55bc01f0f92fc496fae4062
Alberto Basalo <albertobasalo@hotmail.com>
2020-04-24 12:42:05 +0200

    feat: 🤷🏼‍♂️ no convertbox script

--- src/components/core/Scripts.js
+++ src/components/core/Scripts.js
@@ -18,21 +18,21 @@ export default function Scripts(props) {
       <link rel="preconnect" href="https://cdn.jsdelivr.net"></link>
       <link rel="preconnect" href="https://www.googletagmanager.com"></link>
       <link rel="preconnect" href="https://www.google-analytics.com"></link>
-      <script
+      {/* <script
         async
         src={convertBox.src}
         id={convertBox.id}
         type="text/javascript"
         data-uuid={convertBox.data_uuid}
-      />
+      /> */}
     </React.Fragment>
   );
 }
 const cookieScriptUrl =
   'https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?forceLang=es&tracking=1&thirdparty=1&always=1&showPolicyLink=1&privacyPage=https%3A%2F%2Fbitademy.com%2Fpolitics%2Fprivacidad';
 
-const convertBox = {
-  src: 'https://cdn.convertbox.com/convertbox/js/embed.js',
-  id: 'app-convertbox-script',
-  data_uuid: 'b3e703fb-a9f2-41c4-8db3-b114b7899c4c'
-};
+// const convertBox = {
+//   src: 'https://cdn.convertbox.com/convertbox/js/embed.js',
+//   id: 'app-convertbox-script',
+//   data_uuid: 'b3e703fb-a9f2-41c4-8db3-b114b7899c4c'
+// };
